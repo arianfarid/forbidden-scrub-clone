0f24100097ea02a5ecea30867c7946527a0f7334
arianfarid <arian.blank.farid@gmail.com>
2021-09-06 20:34:36 -0400

    game ready state

--- src/components/GamePlayerCard.vue
+++ src/components/GamePlayerCard.vue
@@ -4,25 +4,33 @@
             <div class="rounded-full h-24 w-24 bg-gray-300">
                 <img class="rounded-full h-24 w-24 ring-gray-200 ring-1" v-if="character_img" :src="character_img">
             </div>
-            <div class="justify-center p-2">
+            <div class="p-2">
                 <div>
-                    Name: {{player_name.value}}
+                    Name:
                 </div>
                 <div>
-                    <input ref="player_name" type="text" class="rounded-sm ring-1 ring-gray-200 focus:ring-1 w-32 focus:ring-purple-600 outline-none" v-on:input="updateInput()" @change="updateInput()">
+                    <input ref="player_name" type="text" class="rounded-sm 
+                    ring-1 ring-gray-200 hover:ring-purple-300 focus:ring-1 w-32 focus:ring-purple-600 outline-none" v-on:input="updateName()" @change="updateName()">
                 </div>
             </div>
         </div>
-        <div class="flex flex-row">
+        <div class="flex flex-row pt-4">
             <div>
                 Character:
             </div>
             <div>
-                <select ref="character_selected" type="text" class="rounded-sm ring-1 ring-gray-200 focus:ring-1 ml-2 w-auto focus:ring-purple-600 outline-none" v-on:input="updateInput(), addCharacterImage()" @change="updateInput(), addCharacterImage()">
+                <select ref="character_selected" class="rounded-sm 
+                ring-1 ring-gray-200 hover:ring-purple-300 focus:ring-1 w-32 focus:ring-purple-600 outline-none" v-on:input="updateCharacter(), addCharacterImage()">
                     <option></option>
                     <option v-for="option in characters" v-bind:key="option.id" :value="option.name">{{option.name}}</option>
                 </select>
             </div>
+        </div>
+        <div class="flex flex-row pt-4">
+            Character infos. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
         </div>
     </div>
 </template>
@@ -37,10 +45,15 @@ export default {
         const characters = inject('characters');
         const character_selected = ref('');
         const character_img = ref('');
-        const updateInput = () => {
-            context.emit("playerInputChange", {
+        const updateName = () => {
+            context.emit('playerNameChange', {
                 "id": props.player_data.id,
                 "name": player_name.value.value,
+            });
+        }
+        const updateCharacter = () => {
+            context.emit('playerCharacterChange', {
+                "id": props.player_data.id,
                 "character": character_selected.value.value,
                 "character_id": characters.value[characters.value.findIndex(element => element.name === character_selected.value.value)].id,
                 "character_img": characters.value[characters.value.findIndex(element => element.name === character_selected.value.value)].img,
@@ -55,7 +68,8 @@ export default {
             character_selected,
             characters,
             player_name,
-            updateInput,
+            updateName,
+            updateCharacter,
         }
     }
 };
--- src/components/GameSetUp.vue
+++ src/components/GameSetUp.vue
@@ -1,26 +1,31 @@
 <template>
     <div class="bg-gray-50">
-        <div class="flex text-lg m-2">
+        <div class="flex flex-row text-lg m-2">
             <label>Number of players:</label>
             <div class="flex cursor-pointer">
                 <div class="flex flex-row bg-gray-200 rounded-full ml-2 px-2 ring-gray-500 hover:ring-1 hover:ring-gray-400">
-                    <div @click="setPlayerCount(2), createPlayers(players, players.length, player_count)" class="px-1 hover:bg-gray-300 border-r border-gray-500">2</div>
-                    <div @click="setPlayerCount(3), createPlayers(players, players.length, player_count)" class="px-1 hover:bg-gray-300 border-r border-gray-500">3</div>
-                    <div @click="setPlayerCount(4), createPlayers(players, players.length, player_count)" class="px-1 hover:bg-gray-300 border-r border-gray-500">4</div>
-                    <div @click="setPlayerCount(5), createPlayers(players, players.length, player_count)" class="px-1 hover:bg-gray-300">5</div>
+                    <div @click="setPlayerCount(2), createPlayers(players, players.length, player_count)" class="px-3 hover:bg-gray-300 border-r border-gray-500">2</div>
+                    <div @click="setPlayerCount(3), createPlayers(players, players.length, player_count)" class="px-3 hover:bg-gray-300 border-r border-gray-500">3</div>
+                    <div @click="setPlayerCount(4), createPlayers(players, players.length, player_count)" class="px-3 hover:bg-gray-300 border-r border-gray-500">4</div>
+                    <div @click="setPlayerCount(5), createPlayers(players, players.length, player_count)" class="px-3 hover:bg-gray-300">5</div>
                 </div>
+            </div>
+            <div>
+                <button v-if="game_ready" class="inline-block px-4 py-2 leading-none border rounded text-purple-600 border-purple-600 hover:border-purple-300 hover:text-purple-600 hover:bg-white mt-4 lg:mt-0">
+                    Start Game
+                </button>
             </div>
         </div>
         <div class="flex flex-wrap">
             <div v-for="player in players" v-bind:key="player.id">
-                <game-player-card v-bind:player_data="player" @player-input-change="updatePlayerData($event)"></game-player-card>
+                <game-player-card v-bind:player_data="player" @player-name-change="updatePlayerName($event)" @player-character-change="updatePlayerCharacter($event)"></game-player-card>
             </div>
             {{ players }}
         </div>
     </div>
 </template>
 <script>
-import { inject } from 'vue';
+import { inject, ref, watch } from 'vue';
 import GamePlayerCard from '@/components/GamePlayerCard.vue';
 export default {
     components: {
@@ -30,22 +35,51 @@ export default {
 
     },
     setup() {
+        //////////////////
+        // Player data variables 
+        // and functions
+        //
         const player_count = inject('player_count');
         const setPlayerCount = inject('setPlayerCount');
         const createPlayers = inject('createPlayers');
         const players = inject('players');
-        const updatePlayerData = (event) => {
+        const updatePlayerName = (event) => {
             players.value[event.id - 1].name = event.name;
+        }
+        const updatePlayerCharacter = (event) => {
             players.value[event.id - 1].character = event.character;
             players.value[event.id - 1].character_id = event.character_id;
-            return
         }
+
+        //////////////////
+        // Game state
+        //
+        const game_ready = ref(false);
+        const checkGameReady = () => {
+            if (players.value.some(element => element.character === '') || players.value.some(element => element.name === '')) {
+                return game_ready.value = false;
+            } else {
+                console.log('game ready');
+                return game_ready.value = true;
+            }
+        }
+
+        //////////////////
+        // update game state when any player 
+        // data has been updated
+        //
+        watch(players.value, async () => {
+            checkGameReady();
+        });
         return {
+            checkGameReady,
             createPlayers,
+            game_ready,
             player_count,
             players,
             setPlayerCount,
-            updatePlayerData
+            updatePlayerCharacter,
+            updatePlayerName
         }
     }
 };
