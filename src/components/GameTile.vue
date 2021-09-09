61366b1f3d1c9880f6c06b1b65e34ec8c1df258c
arianfarid <arian.blank.farid@gmail.com>
2021-09-09 18:59:10 -0400

    sand color function to class

--- src/components/GameTile.vue
+++ src/components/GameTile.vue
@@ -1,9 +1,15 @@
 <template>
-    <div @click="advancePlayer()" v-if="!is_vortex" class="grid justify-items-center 
+
+    <div @click="advancePlayer()" 
+    v-if="!is_vortex" 
+    :class="sandVizualize()"
+    class="grid justify-items-center 
     hover:ring-1 hover:ring-purple-600
     text-sm
-    bg-gradient-to-t from-yellow-500 via-yellow-500 to-blue-300
-    p-7 rounded-md shadow-md">
+
+    p-7 rounded-md shadow-md"
+    >
+    <!-- old tile bg     bg-gradient-to-t from-yellow-500 via-yellow-500 to-blue-300 -->
         <div>
             <div v-if="sand_count > 0" class="filter sepia flex flex-grow"></div>
             <p v-if="!crash_site && !is_vortex">
@@ -14,13 +20,16 @@
             </div>
             <div>
                 <div v-for="(position, index) in player_positions" v-bind:key="position" >
-                    <img v-if="position === current_tile.index" class="rounded-full h-7 w-7 ring-gray-200 bg-gray-300 ring-1" :src="players[index].character_img" />
+                    <img v-if="position === current_tile.index" 
+                    class="rounded-full h-7 w-7 ring-gray-200 bg-gray-300 ring-1 m-1" 
+                    :class="currentPlayerClass(index)"
+                    :src="players[index].character_img" />
                 </div>
             </div>
             <br>
-            {{sand_count}} sands
         </div>
     </div>
+
     <div v-if="is_vortex" class="grid justify-items-center 
     text-sm
     bg-opacity-0 
@@ -48,8 +57,24 @@ export default {
         const is_vortex = (props.tile_type === 'Vortex');
         const advancePlayer = inject('advancePlayer');
         const players = inject('players');
+        const currentPlayerClass = (index) => {
+            if(props.current_player_index===index){
+                return 'ring-red-400 ring-2'
+            }
+            return ''
+        };
+        const sandVizualize = () => {
+            if(props.sand_count === 1) {
+                return 'backdrop-filter backdrop-sepia-25'
+            } else if (props.sand_count > 1) {
+                return 'backdrop-filter backdrop-sepia-75'
+            }
+            return 'backdrop-filter backdrop-grayscale backdrop-opacity-40'
+        }
         return {
             advancePlayer,
+            currentPlayerClass,
+            sandVizualize,
             is_vortex,
             players
         }
